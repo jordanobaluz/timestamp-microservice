9c1136547503d313ac6c756216179fe2f53d8094
JordanoBaluz <jordanolb95@hotmail.com>
2022-07-12 22:08:33 -0300

    modified the index.js to process requests URL

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const http = require("http");
+const fs = require("fs");
+
+//require statements
+const getTimestamp = (date) => ({
+  unix: date.getTime(),
+  utc: date.toUTCString(),
+});
 
 const requestHandler = (req, res) => {
   if (req.url === "/") {
+    //read the file provided in the first argument and execute
+    fs.readFile("views/index.html", "utf8", (err, html) => {
+      if (err) throw err;
+      res.writeHead(200, { "content-Type": "text/html" });
+      //sends the content file to the browser
+      res.end(html);
+    });
+  } else if (req.url.startWith("/api/timestamp")) {
+    const dateString = req.url.split("/api/timestamp/")[1];
+    let timestamp;
+    if (dateString === undefined || dateString.trim() === "") {
+      timestamp = getTimestamp(new Date());
+    } else {
+      const date = !isNaN(dateString)
+        ? new Date(parseInt(dateString))
+        : new Date(dateString);
+    }
+    if (!isNaN(date.getTime())) {
+      timestamp = getTimestamp(date);
+    } else {
+      timestamp = {
+        error: "invalid date",
+      };
+    }
+
+    res.writeHead(200, { "content-Type": "application/json" });
+    res.end(JSON.stringify(timestamp));
   }
-  console.log(req.url);
-  res.end("Hello world!");
 };
 
 const server = http.createServer(requestHandler);
